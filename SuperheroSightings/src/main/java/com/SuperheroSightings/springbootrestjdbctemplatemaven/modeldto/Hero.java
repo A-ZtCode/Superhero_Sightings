3893181eed7eae5853e218696b1bd3f95d30a0f8
AltheaZeta <86517586+AltheaZeta@users.noreply.github.com>
2023-09-13 16:26:51 +0100

    Update Hero.java

--- SuperheroSightings/src/main/java/com/SuperheroSightings/springbootrestjdbctemplatemaven/modeldto/Hero.java
+++ SuperheroSightings/src/main/java/com/SuperheroSightings/springbootrestjdbctemplatemaven/modeldto/Hero.java
@@ -1,56 +1,17 @@
 package com.SuperheroSightings.springbootrestjdbctemplatemaven.modeldto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Hero {
     private int id;
     private String name;
     private String description;
     private String superpower;
-
-    // Constructor with parameters
-    public Hero(int id, String name, String description, String superpower) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.superpower = superpower;
-    }
-
-    // Default constructor
-    public Hero() {}
-
-    // getters, setters, and other methods
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSuperpower() {
-        return superpower;
-    }
-
-    public void setSuperpower(String superpower) {
-        this.superpower = superpower;
-    }
 }
